6a76e865ea384e8a657c95140903b8cf2d98d052
brianrono <brianrono16@gmail.com>
2023-04-24 18:51:13 +0300

    To finish

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,16 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions}) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */
+          questions.map((question) => (
+            <QuestionItem question={question} key={question.id} />
+          ))
+        }
+      </ul>
     </section>
   );
 }
